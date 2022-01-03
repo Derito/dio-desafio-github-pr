c0e792d7f7c30c5bd4d83956b3a663c3f5b59747
Derito <helderdacsta@hotmail.com>
2022-01-03 20:52:12 +0100

    Inserção ecxercicios sobre casting

--- ExercicoVariavaeisDio/.idea/sonarlint/issuestore/index.pb
+++ ExercicoVariavaeisDio/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ U
 %src/org/exercicio/dio/Operadores.java,8\b\8baede2cd65c68af0b6c285111489b48d2759b98
 P
  src/org/exercicio/dio/Tipos.java,d\0\d02e890a550ee39d157e8e6257ac4ed35a9439f3
+R
+"src/org/exercicio/dio/Casting.java,e\9\e9550683b267ca18911cb0312524dbbf74cd1351
---
+++ ExercicoVariavaeisDio/src/org/exercicio/dio/Casting.java
@@ -0,0 +1,49 @@
+package org.exercicio.dio;
+
+public class Casting {
+
+    public static void main(String[] args) {
+
+        int i = 8;
+        long a = 234568;
+        byte b = 22;
+        float c = 2.3f;
+        double d = 2.7777777d;
+
+        // int
+        i = (int) a;// Necessário casting porque estamos atribuindo a uma variavel int um long > capacidade
+        a = i; // Não necessita casting por a ser long e i int menor capacidade
+        i = b;// Não necessita casting por i ser int e b byte menor capacidade
+        b = (byte) i; // Necessário casting porque estamos atribuindo a uma variavel byte um int > capacidade
+        i = (int) c; // Necessário casting porque estamos atribuindo a uma variavel int um float > capacidade
+        c = i;// Não necessita casting por c ser float e i int menor capacidade
+        i = (int) d;// Necessário casting porque estamos atribuindo a uma variavel int um double > capacidade
+        d = i;// Não necessita casting por d ser double e i int menor capacidade
+
+        //Long
+        a = b;//Não necessita casting por a ser long e b byte menor capacidade
+        b = (byte) a;// Necessário casting porque estamos atribuindo a uma variavel byte um long > capacidade
+        a = (long) c;// Necessário casting porque estamos atribuindo a uma variavel byte um float > capacidade
+        c = a;//Não necessita casting por c ser float e a long menor capacidade
+        a = (long) d;// Necessário casting porque estamos atribuindo a uma variavel long um double > capacidade
+        d = a;//Não necessita casting por d ser double e a long menor capacidade
+
+        //byte
+        b = (byte) c;// Necessário casting porque estamos atribuindo a uma variavel byte um float > capacidade
+        c = b;//Não necessita casting por c ser float e b byte menor capacidade
+        b = (byte) d;// Necessário casting porque estamos atribuindo a uma variavel byte um double > capacidade
+        d = b;//Não necessita casting por d ser double e b byte menor capacidade
+
+        //float e double
+        c = (float) d;// Necessário casting porque estamos atribuindo a uma variavel float um double > capacidade
+        d = c;//Não necessita casting por d ser double e c float menor capacidade
+
+        //Exemplos
+        System.out.println(i);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
