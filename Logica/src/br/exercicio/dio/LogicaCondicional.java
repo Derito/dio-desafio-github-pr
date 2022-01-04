56e9dfaeac9afef4c1622d58e0935ef0431e3410
Derito <helderdacsta@hotmail.com>
2022-01-04 15:53:38 +0100

    Adição de exercicios dio

--- Logica/src/br/exercicio/dio/LogicaCondicional.java
+++ Logica/src/br/exercicio/dio/LogicaCondicional.java
@@ -43,9 +43,37 @@ public class LogicaCondicional {
         }
 
         // 2
-        if(resultado == 1 || resultado == 7 || resultado == 12){
+        if (resultado == 1 || resultado == 7 || resultado == 12) {
             System.out.println("Ferias");
         }
 
+        Scanner scan = new Scanner(System.in);
+        String diaSemana = scan.next();
+
+        switch (diaSemana) {
+            case "Segunda":
+                System.out.println("2 ");
+                break;
+            case "Terça":
+                System.out.println("3");
+                break;
+            case "Quarta":
+                System.out.println("4");
+                break;
+            case "Quinta":
+                System.out.println("5");
+                break;
+            case "Sexta":
+                System.out.println("6");
+                break;
+            case "Sabado":
+                System.out.println("7 " );
+                break;
+            case "Domingo":
+                System.out.println("1");
+                break;
+            default:
+                System.out.println("Dia não existente");
+        }
     }
 }
