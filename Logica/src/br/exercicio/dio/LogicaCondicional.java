ebffbd62496ab95c5c3c385eda91f4301f8671e6
Derito <helderdacsta@hotmail.com>
2022-01-04 16:31:45 +0100

    Melhorando o código.

--- Logica/src/br/exercicio/dio/LogicaCondicional.java
+++ Logica/src/br/exercicio/dio/LogicaCondicional.java
@@ -6,75 +6,45 @@ public class LogicaCondicional {
 
     public static void main(String[] args) {
 
-        //Condicional ternário
-        //Meses de 31 dias Janeiro, Março, Maio, Julho, Agosto, Outubro, Dezembro
-        //Meses de 30 dias Abril, Junho, Setembro, Novembro
-        // 20/29 Fevereiro
+        usandoIf();
+        usandoSwitchVar();
+        usandoIfInc();
+        usandoSwitch();
 
+    }
+    private static void usandoIf() {
         Scanner scanner = new Scanner(System.in);
-        int resultado = scanner.nextInt();
+        int mesDoAno = scanner.nextInt();
         // If para expressões booleanas
-        if (resultado == 1) {
+        if (mesDoAno == 1) {
             System.out.println("Janeiro");
-        } else if (resultado == 2) {
+        } else if (mesDoAno == 2) {
             System.out.println("Fevereiro ");
-        } else if (resultado == 3) {
+        } else if (mesDoAno == 3) {
             System.out.println("Março ");
-        } else if (resultado == 4) {
+        } else if (mesDoAno == 4) {
             System.out.println("Abril ");
-        } else if (resultado == 5) {
+        } else if (mesDoAno == 5) {
             System.out.println("Maio ");
-        } else if (resultado == 6) {
+        } else if (mesDoAno == 6) {
             System.out.println("Junho ");
-        } else if (resultado == 7) {
+        } else if (mesDoAno == 7) {
             System.out.println("Julho ");
-        } else if (resultado == 8) {
+        } else if (mesDoAno == 8) {
             System.out.println("Agosto ");
-        } else if (resultado == 9) {
+        } else if (mesDoAno == 9) {
             System.out.println("Setembro ");
-        } else if (resultado == 10) {
+        } else if (mesDoAno == 10) {
             System.out.println("Outubro ");
-        } else if (resultado == 11) {
+        } else if (mesDoAno == 11) {
             System.out.println("Novembro ");
-        } else if (resultado == 12) {
+        } else if (mesDoAno == 12) {
             System.out.println("Dezembro ");
         } else {
             System.out.println("Valor não existente ");
         }
-
-        // 2
-        if (resultado == 1 || resultado == 7 || resultado == 12) {
-            System.out.println("Ferias");
-        }
-
-        Scanner scan = new Scanner(System.in);
-        String diaSemana = scan.next();
-
-        switch (diaSemana) {
-            case "Segunda":
-                System.out.println("2 ");
-                break;
-            case "Terça":
-                System.out.println("3");
-                break;
-            case "Quarta":
-                System.out.println("4");
-                break;
-            case "Quinta":
-                System.out.println("5");
-                break;
-            case "Sexta":
-                System.out.println("6");
-                break;
-            case "Sabado":
-                System.out.println("7 ");
-                break;
-            case "Domingo":
-                System.out.println("1");
-                break;
-            default:
-                System.out.println("Dia não existente");
-        }
+    }
+    private static void usandoSwitchVar() {
         Scanner scane = new Scanner(System.in);
         int variavel = scane.nextInt();
 
@@ -92,6 +62,42 @@ public class LogicaCondicional {
             default:
                 System.out.println("Fora do rating ou valor indefinido!");
                 break;
+        }
+    }
+    private static void usandoIfInc(){
+        Scanner scan = new Scanner(System.in);
+         int mesDoAno = scan.nextInt();
+        if (mesDoAno == 1 || mesDoAno == 7 || mesDoAno == 12) {
+            System.out.println("Ferias");
+        }
+    }
+    private static void usandoSwitch() {
+        Scanner scan = new Scanner(System.in);
+        String diaSemana = scan.next();
+        switch (diaSemana) {
+            case "Segunda":
+                System.out.println(2);
+                break;
+            case "Terça":
+                System.out.println(3);
+                break;
+            case "Quarta":
+                System.out.println(4);
+                break;
+            case "Quinta":
+                System.out.println(5);
+                break;
+            case "Sexta":
+                System.out.println(6);
+                break;
+            case "Sabado":
+                System.out.println(7);
+                break;
+            case "Domingo":
+                System.out.println(1);
+                break;
+            default:
+                System.out.println("Dia não existente");
         }
     }
 }
