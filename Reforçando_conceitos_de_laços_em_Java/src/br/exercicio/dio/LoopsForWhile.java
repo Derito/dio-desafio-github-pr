20cccdf69911503c7b2b7da41c7edf7f74f4d78e
Derito <helderdacsta@hotmail.com>
2022-01-07 10:21:49 +0100

    Reforçando conceitos de laços- em java

--- Reforçando_conceitos_de_laços_em_Java/src/br/exercicio/dio/LoopsForWhile.java
+++ Reforçando_conceitos_de_laços_em_Java/src/br/exercicio/dio/LoopsForWhile.java
@@ -5,7 +5,9 @@ public class LoopsForWhile {
         //loopFor();
         //lacoWhile();
         // forEBreak();
-        whileEBreak();
+        //whileEBreak();
+        diversos();
+        //usandoForEach();
     }
 
     private static void loopFor() {
@@ -53,6 +55,70 @@ public class LoopsForWhile {
             if (cont == 12) break aborta;
             cont++;
 
+        }
+
+    }
+
+    private static void diversos() {
+
+        int[] idade = new int[10];
+        idade[0] = 17;
+        idade[1] = 45;
+
+        System.out.println("Idade na posição 0: " + idade[0]);
+        System.out.println("Idade na posição 1: " + idade[1]);
+
+        //Ou
+        int[] idade1 = {20, 5};
+        System.out.println();
+        System.out.println("Idade na posição 0: " + idade1[0]);
+        System.out.println("Idade na posição 1: " + idade1[1]);
+
+        //Ou
+        int[] idade2 = new int[]{30, 19};
+        System.out.println();
+        System.out.println("Idade na posição 0: " + idade2[0]);
+        System.out.println("Idade na posição 1: " + idade2[1]);
+
+        //Ou
+        int[] idade3 = {7, 9};
+        System.out.println();
+        System.out.println("Idade na posição 0: " + idade3[0]);
+        System.out.println("Idade na posição 1: " + idade3[1]);
+
+        //char
+        char[] ch = new char[3];
+        ch = new char[]{'a','g','4'};
+
+        System.out.println();
+        for(char i : ch){
+            System.out.println("Valores do char: " + i);
+        }
+        //long
+        long[] num = {1234,785645};
+        System.out.println();
+        for(long k : num){
+            System.out.println("Valores do long: " + k);
+        }
+        //String
+        String[] letra = {"água","leite","vinho"};
+        System.out.println();
+        for(String l : letra){
+            System.out.println("Valores do long: " + l);
+        }
+    }
+
+    private static void usandoForEach() {
+
+        int[] idades = new int[10];
+        idades = new int[]{1, 4, 6, 23, 45, 67, 12, 34, 2, 8};
+        for (int idade : idades) {
+            System.out.println("Idades existentes no array: " + idade);
+        }
+        //Mesmo exercicio usando for normal
+        System.out.println();
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println("Idades existentes no array: " + i);
         }
     }
 }
